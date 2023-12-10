f75673de12919cc18b5fe7e51ef44bd384069497
ngM0de <tillic0llapse@protonmail.com>
2023-12-10 22:10:08 +0200

    feat: add route, add redirect to app router

--- app.router.js
+++ app.router.js
@@ -3,7 +3,12 @@ import express from "express";
 const appRouter = express.Router();
 
 appRouter.get('/', (req, res) => {
-    res.json(`Just test message`)
+    res.json(`This is root route`)
 })
+appRouter.get('/page', (req, res) => {
+    res.json(`This is page route`)
+})
+
+
 
 export {appRouter}
